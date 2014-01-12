53478f47ae61cae170b1b5f05054a6336b9ffba9
new-eyes-open <mhiggins1785@gmail.com>
2014-01-12 15:43:57 -0800

    Tip Calculator
    A tip calculator that displays in dialog boxes.

---
+++ gratuityCalc.java
@@ -0,0 +1,28 @@
+import javax.swing.JOptionPane;
+
+public class gratuityCalc {
+  public static void main(String[] args) {
+	
+	//Dialog box prompting bill amount and input conversion to double
+	String subTotalString = JOptionPane.showInputDialog(
+		"Enter bill subtotal, for example, 12.50");
+	double subTotal = Double.parseDouble(subTotalString);
+	
+	//Dialog box prompting tip rate and input conversion to double
+	String tipPercentString = JOptionPane.showInputDialog(
+		"Enter the percent you intend to tip, for example, 15");
+	double tipPercent = Double.parseDouble(tipPercentString);
+	
+	//Convert tip to decimal
+	tipPercent = tipPercent / 100;
+	
+	//Calculate gratuity and total bill
+	double gratuityAmount = subTotal * tipPercent;
+	double totalBill = subTotal + gratuityAmount;
+	
+	//Display amounts
+	String output = "The tip should be $" + gratuityAmount + " which makes the total $" +
+		totalBill;
+	JOptionPane.showMessageDialog(null, output);
+	}
+}
